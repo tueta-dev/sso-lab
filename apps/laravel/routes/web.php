5df2582a8ca11850521198818d6701c3418fc6ca
ueta_taiki <taixizhitian@gmail.com>
2025-11-24 10:11:08 +0900

    feat: add routes for login and callback

--- apps/laravel/routes/web.php
+++ apps/laravel/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\CallbackController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/login', LoginController::class)->name('auth.login');
+
+Route::get('/auth/callback', CallbackController::class)->name('auth.callback');
